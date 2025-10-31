b710d061f07cca96a23e3e08bbac27f4ecdd5c7e
yatankek <ninja_mos@mail.ru>
2025-10-31 22:01:38 +0300

    Добавление главного экрана и экрана блюд

---
+++ lib/features/menu/presentation/screens/dish_detail_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:prac6/features/menu/data/models/dish_model.dart';
+
+class DishDetailScreen extends StatelessWidget {
+  final Dish dish;
+
+  const DishDetailScreen({
+    super.key,
+    required this.dish,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(dish.name),
+        backgroundColor: const Color(0xFFD32F2F),
+        foregroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          CachedNetworkImage(
+            imageUrl: dish.imageUrl,
+            width: double.infinity,
+            height: 250,
+            fit: BoxFit.cover,
+            placeholder: (context, url) => Container(
+              height: 250,
+              color: Colors.grey[200],
+              child: const Icon(Icons.restaurant, size: 50, color: Colors.grey),
+            ),
+            errorWidget: (context, url, error) => Container(
+              height: 250,
+              color: Colors.red[100],
+              child: const Icon(Icons.error, size: 50, color: Colors.red),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  dish.name,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  dish.description,
+                  style: const TextStyle(fontSize: 16, color: Colors.grey),
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  dish.formattedPrice,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFFD32F2F),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
